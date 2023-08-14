d0af31c31cf85a172f5a498c3f2daf5501c0c4ae
MostafaHassan724 <mostafahassan725@gmail.com>
2023-08-14 21:52:02 +0300

    Add configuration for django

--- djangoProject/djangoRoot/settings.py
+++ djangoProject/djangoRoot/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'djangoProject.listingIPApp'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,12 +76,15 @@ WSGI_APPLICATION = 'djangoRoot.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'mydb',
+            'USER': os.environ.get['DB_USERNAME'],
+            'PASSWORD': os.environ.get['DB_PASSWORD'],
+            'HOST': os.environ.get['DB_ENDPOINT'],
+            'PORT': 3306,
+        }
     }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- djangoProject/listingIPApp/models.py
+++ djangoProject/listingIPApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class IPAdresses(models.Model):
+    ip_address = models.GenericIPAddressField()
+    date_added = models.DateTimeField()
