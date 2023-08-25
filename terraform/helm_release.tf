08fc215af02766e2a24c11ce4e9735edf104b6ca
Mostafa Hassan <30627787+mostafahassan725@users.noreply.github.com>
2023-08-26 00:02:11 +0300

    Update helm_release.tf

--- terraform/helm_release.tf
+++ terraform/helm_release.tf
@@ -3,8 +3,8 @@
 resource "helm_release" "django" {
   name             = var.helm_release_name
   chart            = var.chart_path
-  namespace        = var._release_namespace
-  max_history      = var.maxhistory
+  namespace        = var.release_namespace
+  max_history      = var.max_history
   wait             = true
   lint             = true
   reset_values     = true
