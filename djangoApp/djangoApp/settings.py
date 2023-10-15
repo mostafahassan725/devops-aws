ce2fddde07838c8e77c32061608327730e72d412
MostafaHassan724 <mostafahassan725@gmail.com>
2023-10-15 06:21:14 +0300

    modified

--- djangoApp/djangoApp/settings.py
+++ djangoApp/djangoApp/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--mc^y$2x)83e3hu)4z5x)!bm$42^e-0dp^f8$s!&vxq&e-fobt'
+# SECRET_KEY = 'django-insecure--mc^y$2x)83e3hu)4z5x)!bm$42^e-0dp^f8$s!&vxq&e-fobt'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,6 +84,15 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    #   'default': {
+    #       'ENGINE': 'django.db.backends.mysql',
+    #       'NAME': os.environ.get('DATABASE_NAME'),
+    #       'USER': os.environ.get('DATABASE_USER'),
+    #       'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+    #       'HOST': os.environ.get('DATABASE_HOST'),
+    #       'PORT': os.environ.get('DATABASE_PORT'),
+    #    },
+
 }
 
 
